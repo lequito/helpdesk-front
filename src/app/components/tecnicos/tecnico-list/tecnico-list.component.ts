486d1258d526d9b31cb8df5dc590ade08b69f9c1
lequito <=>
2022-02-16 05:15:49 -0300

    criação do component e

---
+++ src/app/components/tecnicos/tecnico-list/tecnico-list.component.ts
@@ -0,0 +1,42 @@
+import { Component, OnInit, ViewChild } from '@angular/core';
+import { MatPaginator } from '@angular/material/paginator';
+import { MatTableDataSource } from '@angular/material/table';
+import { Tecnico } from 'src/app/models/tecnico';
+
+
+@Component({
+  selector: 'app-tecnico-list',
+  templateUrl: './tecnico-list.component.html',
+  styleUrls: ['./tecnico-list.component.css']
+})
+export class TecnicoListComponent implements OnInit {
+  
+  ELEMENT_DATA: Tecnico[] = [
+    {
+      id: 1,
+      nome: 'Alex Dias',
+      cpf: '123.456.789-10',
+      email: 'alex@mail.com',
+      senha: '123',
+      perfis: ['0'],
+      dataCriacao: '16/07/22'
+    }
+  ]
+
+  displayedColumns: string[] = ['id', 'nome', 'cpf', 'email', 'acoes'];
+  dataSource = new MatTableDataSource<Tecnico>(this.ELEMENT_DATA);
+  
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+
+  @ViewChild(MatPaginator) paginator: MatPaginator;
+
+  ngAfterViewInit() {
+    this.dataSource.paginator = this.paginator;
+  }
+
+}
+
